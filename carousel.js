aa53be444b48bb1bc25f4ef6e8a9d747714c0de8
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2022-07-15 03:57:19 +0400

    added carousel, fixed some stuff for Dania

---
+++ carousel.js
@@ -0,0 +1,44 @@
+let sliderContainer = document.querySelector('.carousel');
+let innerSlider = document.querySelector('.carousel_inner');
+
+let pressed = false;
+let startX;
+let x;
+
+sliderContainer.addEventListener("mousedown", (e) => {
+    pressed = true;
+    startX = e.offsetX - innerSlider.offsetLeft;
+    sliderContainer.style.cursor = "grabbing";
+});
+
+sliderContainer.addEventListener("mouseenter", () => {
+    sliderContainer.style.cursor = "grab";
+});
+
+sliderContainer.addEventListener("mouseup", () => {
+    sliderContainer.style.cursor = "grab";
+    pressed = false;
+});
+
+
+const checkBoundary = () => {
+    let outer = sliderContainer.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = "0px";
+    }
+
+    if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`;
+    }
+};
+
+sliderContainer.addEventListener("mousemove", (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX;
+    innerSlider.style.left = `${x - startX}px`;
+    checkBoundary();
+});
