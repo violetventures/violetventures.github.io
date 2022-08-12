58a47d19afda3de12165d48a24fa8fda88b7655e
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2022-08-12 07:17:03 +0400

    fixes

---
+++ cards.js
@@ -0,0 +1,9 @@
+$("#speaker1").click(function() {
+    $("#overlay-speaker1").toggleClass("hidden");
+
+}
+);
+
+$("#close-speaker1").click(function() {
+    $("#overlay-speaker1").toggleClass("hidden");
+});
