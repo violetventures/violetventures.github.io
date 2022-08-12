0a68cb891711952bf1ee54a0e41c5bdf5f9a7da6
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2022-08-12 09:42:50 +0400

    added mobile menu

---
+++ menu.js
@@ -0,0 +1,20 @@
+$("#openmenu").click(function(){
+    $("#closemenu").toggleClass("hidden");
+    $("#openmenu").toggleClass("hidden");
+    $("#fullmenu").toggleClass("hidden");
+}
+);
+
+$("#closemenu").click(function(){
+    $("#closemenu").toggleClass("hidden");
+    $("#openmenu").toggleClass("hidden");
+    $("#fullmenu").toggleClass("hidden");
+}
+);
+
+$(".mobile-menuitem").click(function(){
+    $("#closemenu").toggleClass("hidden");
+    $("#openmenu").toggleClass("hidden");
+    $("#fullmenu").toggleClass("hidden");
+}
+);
