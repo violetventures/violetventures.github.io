6faa87e22a02b66e8e5751a650e8bd158219fb33
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2022-09-29 13:56:40 +0200

    major content changes #1

--- cards.js
+++ cards.js
@@ -1,9 +1,16 @@
+
 $("#speaker1").click(function() {
     $("#overlay-speaker1").toggleClass("hidden");
-
-}
-);
+});
 
 $("#close-speaker1").click(function() {
     $("#overlay-speaker1").toggleClass("hidden");
+});
+
+$("#speaker2").click(function() {
+    $("#overlay-speaker2").toggleClass("hidden");
+});
+
+$("#close-speaker2").click(function() {
+    $("#overlay-speaker2").toggleClass("hidden");
 });
