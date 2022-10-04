3c545ae981db7a43e0ab09a00241e1baea3e632e
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2022-10-04 12:42:30 +0200

    lots of progress

--- cards.js
+++ cards.js
@@ -14,3 +14,43 @@ $("#speaker2").click(function() {
 $("#close-speaker2").click(function() {
     $("#overlay-speaker2").toggleClass("hidden");
 });
+
+$("#speaker3").click(function() {
+    $("#overlay-speaker3").toggleClass("hidden");
+});
+
+$("#close-speaker3").click(function() {
+    $("#overlay-speaker3").toggleClass("hidden");
+});
+
+$("#speaker4").click(function() {
+    $("#overlay-speaker4").toggleClass("hidden");
+});
+
+$("#close-speaker4").click(function() {
+    $("#overlay-speaker4").toggleClass("hidden");
+});
+
+$("#speaker5").click(function() {
+    $("#overlay-speaker5").toggleClass("hidden");
+});
+
+$("#close-speaker5").click(function() {
+    $("#overlay-speaker5").toggleClass("hidden");
+});
+
+$("#speaker6").click(function() {
+    $("#overlay-speaker6").toggleClass("hidden");
+});
+
+$("#close-speaker6").click(function() {
+    $("#overlay-speaker6").toggleClass("hidden");
+});
+
+$("#speaker7").click(function() {
+    $("#overlay-speaker7").toggleClass("hidden");
+});
+
+$("#close-speaker7").click(function() {
+    $("#overlay-speaker7").toggleClass("hidden");
+});
